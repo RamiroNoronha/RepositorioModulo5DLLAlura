a7e66f8f543f02c61c8765b04bea425536afb221
RamiroNoronha <100360738+RamiroNoronha@users.noreply.github.com>
2022-03-16 21:10:59 -0300

    Finalização da aula 2 do módulo 5 de DLL

---
+++ ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.Modelos.Funcionarios
+{
+    public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+    {
+        private AutenticarHelper _autenticarHelper = new AutenticarHelper();
+        public string Senha { get; set; }
+
+        public FuncionarioAutenticavel(double salario, string cpf)
+            : base(salario, cpf)
+        {
+        }
+
+        public bool Autenticar(string senha)
+        {
+            return _autenticarHelper.CompararSenhar(Senha, senha);
+        }
+    }
+}
